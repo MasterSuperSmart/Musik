98a2917a3ef026c7168552754ec8ffc539b3c88c
MasterSuperSmart <73879268+MasterSuperSmart@users.noreply.github.com>
2020-11-03 10:57:18 +0100

    Kompasnadel

--- main.ts
+++ main.ts
@@ -1,17 +1,3 @@
-input.onPinPressed(TouchPin.P0, function () {
-    basic.setLedColor(Colors.Red)
-})
-input.onPinPressed(TouchPin.P3, function () {
-    basic.setLedColor(basic.rgbw(
-    255,
-    122,
-    255,
-    0
-    ))
-})
-input.onPinPressed(TouchPin.P2, function () {
-    basic.setLedColor(Colors.Blue)
-})
-input.onPinPressed(TouchPin.P1, function () {
-    basic.setLedColor(Colors.Indigo)
+basic.forever(function () {
+    basic.showCompass(5000)
 })
